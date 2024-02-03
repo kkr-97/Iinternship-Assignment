2c0a3c82777d44d00e0e6b639caf0f570b835b46
kkr-97 <kollikeerthan07@gmail.com>
2024-02-03 12:23:14 +0530

    Updated with effects and animations

---
+++ index.js
@@ -0,0 +1,28 @@
+function onSubmitConsultationForm(){
+    var firstName = document.getElementById("firstName");
+    var lastName = document.getElementById("lastName");
+    var emailAddress = document.getElementById("emailAddress");
+    var phoneNumber = document.getElementById("phoneNumber");
+    var message = document.getElementById("message");
+    var address = document.getElementById("address");
+
+    function validateInputs(){
+        if (firstName.value == "" || lastName.value == "" || emailAddress.value == "" || phoneNumber.value == "" || message.value == "" || address.value == "") {
+            document.getElementById("errMsg").style.display = "flex";
+            return false;
+        }
+        document.getElementById("errMsg").style.display = "none";
+        return true;
+    }
+
+    if(validateInputs()) {
+            firstName.value = "";
+            lastName.value = "";
+            emailAddress.value = "";
+            phoneNumber.value = "";
+            message.value = "";
+            address.value = "";
+
+            alert('Your consultation request has been sent successfully!');
+        }
+}
